36bc076af721a2a972ff726b07c9453cb5c04468
Ricardo Rubio <ricardo.rubio@utp.ac.pa>
2018-12-05 12:27:19 -0500

    POM implemented

--- SampleProjects/POMProjectDemo/Tests/login.py
+++ SampleProjects/POMProjectDemo/Tests/login.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 import time
 import unittest
+from SampleProjects.POMProjectDemo.Pages.loginPage import LoginPage
+from SampleProjects.POMProjectDemo.Pages.homePage import HomePage
 
 class LoginTest(unittest.TestCase):
+
+    @classmethod
     def setUpClass(cls):
 
         cls.driver = webdriver.Chrome(executable_path="C:/Users/r-2g-/PycharmProjects/untitled/drivers/chromedriver.exe")
@@ -10,12 +14,27 @@ class LoginTest(unittest.TestCase):
         cls.driver.maximize_window()
 
     def test_login_valid(self):
+        driver = self.driver
 
-driver.get("https://opensource-demo.orangehrmlive.com/")
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        login = LoginPage(driver)
 
-driver.find_element_by_id("txtUsername").send_keys("Admin")
-driver.find_element_by_id("txtPassword").send_keys("admin123")
-driver.find_element_by_id("btnLogin").click()
-driver.find_element_by_id("welcome").click()
-driver.find_element_by_id("Logout").click()
-time.sleep(2)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+        assert()
+
+        homepage = HomePage(driver)
+        homepage.click_welcome()
+        homepage.click_logout()
+        assert ()
+        time.sleep(2)
+
+        @classmethod
+        def tearDownClass(cls):
+            cls.driver.close()
+            cls.driver.quit()
+            print("tests completed")
+
+
+
